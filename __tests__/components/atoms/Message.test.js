432950f6d84f5f643ba0e58319cd759822adab27
yoat <yoatnic@me.com>
2018-07-31 19:44:50 +0900

    fix component test use enzime

--- __tests__/components/atoms/Message.test.js
+++ __tests__/components/atoms/Message.test.js
@@ -1,11 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import "jest-styled-components";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import Message from "components/atoms/Message";
 
+configure({ adapter: new Adapter() });
+
 test("show message", () => {
-  const tree = renderer.create(<Message message="test message" />).toJSON();
-  expect(tree.children[0]).toEqual("test message");
+  const message = shallow(<Message message="test message" />);
+  expect(message.dive().text()).toEqual("test message");
 });
 
 test("snapshot", () => {
