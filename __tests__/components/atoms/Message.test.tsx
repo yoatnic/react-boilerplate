ed7dee1b1aa27e54bf42eb42a9a2e556c109fd8c
yoat <yoatnic@me.com>
2019-03-10 15:05:27 +0900

    update modules

--- __tests__/components/atoms/Message.test.tsx
+++ __tests__/components/atoms/Message.test.tsx
@@ -1,17 +1,12 @@
 import * as React from "react";
 import * as renderer from "react-test-renderer";
 import "jest-styled-components";
-import { shallow, configure } from "enzyme";
+import { configure } from "enzyme";
 // HACK see: https://github.com/airbnb/enzyme/pull/1264
 const Adapter = require("enzyme-adapter-react-16");
 import Message from "../../../src/components/atoms/Message";
 
 configure({ adapter: new Adapter() });
-
-test("show message", () => {
-  const message = shallow(<Message message="test message" />);
-  expect(message.dive().text()).toEqual("test message");
-});
 
 test("snapshot", () => {
   const tree = renderer.create(<Message message="test message" />).toJSON();
